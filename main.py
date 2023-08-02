575e46c861b14c7236b92cc9b52007f59fe10162
Gabriel da Cruz Silva <gabrielcruzsil@gmail.com>
2023-08-02 11:07:00 -0300

    Fazendo o primeiro programa no python

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    n1 = int(input('Digite o primeiro número: '))
+    n2 = int(input('Digite o segundo número: '))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    resultado = n1 + n2
+
+    print(f'O resultado da soma é {resultado}')
